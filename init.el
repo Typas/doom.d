b4c08de03c3e24edcdb9cbc21956f9886f82be6f
Typas Liao <typascake@gmail.com>
2023-01-04 23:14:32 +0800

    add tree-sitter to languages

--- init.el
+++ init.el
@@ -114,7 +114,7 @@
        :lang
        ;; agda               ; types of types of types of types...
        ;; beancount          ; mind the GAAP
-       (cc +lsp)          ; C/C++/Obj-C madness
+       (cc +lsp +tree-sitter)          ; C/C++/Obj-C madness
        ;; clojure            ; java with a lisp
        ;; common-lisp        ; if you've seen one lisp, you've seen them all
        ;; coq                ; proofs-as-programs
@@ -140,7 +140,7 @@
        ;; json               ; At least it ain't XML
        ;; (java +lsp)        ; the poster child for carpal tunnel syndrome
        ;; javascript         ; all(hope(abandon(ye(who(enter(here))))))
-       julia              ; a better, faster MATLAB
+       (julia +tree-sitter)              ; a better, faster MATLAB
        ;; kotlin             ; a better, slicker Java(Script)
        (latex +lsp)       ; writing papers in Emacs has never been so fun
        ;; lean               ; for folks with too much to prove
@@ -154,7 +154,7 @@
        ;; php                ; perl's insecure younger brother
        ;; plantuml           ; diagrams for confusing people more
        ;; purescript         ; javascript, but functional
-       (python +lsp +pyright)      ; beautiful is better than ugly
+       (python +lsp +pyright +tree-sitter)      ; beautiful is better than ugly
        ;; qt                 ; the 'cutest' gui framework ever
        ;; racket             ; a DSL for DSLs
        ;; raku               ; the artist formerly known as perl6
@@ -164,7 +164,7 @@
        (rust +lsp)        ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;; scala              ; java, but good
        ;; scheme             ; a fully conniving family of lisps
-       sh                 ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +tree-sitter)                ; she sells {ba,z,fi}sh shells on the C xor
        ;; sml
        ;; solidity           ; do you need a blockchain? No.
        ;; swift              ; who asked for emoji variables?
