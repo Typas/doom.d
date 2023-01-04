5634bdd780565e2d4b15f743d5a97f694e5f85d0
Typas Liao <typascake@gmail.com>
2023-01-04 23:47:42 +0800

    add some hope, remove some useless

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
        hl-todo            ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;; hydra
        ;; indent-guides      ; highlighted indent columns
-       ligatures          ; ligatures and symbols to make your code pretty again
+       ;; ligatures          ; ligatures and symbols to make your code pretty again
        ;; minimap            ; show a map of the code on the side
        modeline           ; snazzy, Atom-inspired modeline, plus API
        ;; nav-flash          ; blink cursor line after big motions
@@ -97,7 +97,7 @@
        ;; ein                ; tame Jupyter notebooks with emacs
        (eval +overlay)    ; run code, run (also, repls)
        ;; gist               ; interacting with github gists
-       lookup             ; navigate your code and its documentation
+       ;; lookup             ; navigate your code and its documentation
        lsp                ; M-x vscode
        magit              ; a git porcelain for Emacs
        ;; make               ; run make tasks from Emacs
@@ -128,29 +128,29 @@
        ;; erlang             ; an elegant language for a more civilized age
        ;; ess                ; emacs speaks statistics
        ;; faust              ; dsp, but you get to keep your soul
-       ;; fortran            ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       fortran            ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;; fsharp             ; ML stands for Microsoft's Language
        ;; fstar              ; (dependent) types and (monadic) effects and Z3
        ;; gdscript           ; the language you waited for
-       ;; (go +lsp)          ; the hipster dialect
+       (go +tree-sitter)                 ; the hipster dialect
        ;; (graphql +lsp)     ; Give queries a REST
        ;; (haskell +lsp)     ; a language that's lazier than I am
        ;; hy                 ; readability of scheme w/ speed of python
        ;; idris              ; a language you can depend on
-       ;; json               ; At least it ain't XML
+       (json +tree-sitter)               ; At least it ain't XML
        ;; (java +lsp)        ; the poster child for carpal tunnel syndrome
-       ;; javascript         ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +tree-sitter)         ; all(hope(abandon(ye(who(enter(here))))))
        (julia +tree-sitter)              ; a better, faster MATLAB
        ;; kotlin             ; a better, slicker Java(Script)
        (latex +lsp)       ; writing papers in Emacs has never been so fun
        ;; lean               ; for folks with too much to prove
        ;; ledger             ; be audit you can be
-       lua                ; one-based indices? one-based indices
+       (lua +tree-sitter)                ; one-based indices? one-based indices
        markdown           ; writing docs for people to ignore
        ;; nim                ; python + lisp at the speed of c
        ;; nix                ; I hereby declare "nix geht mehr!"
        ;; ocaml              ; an objective camel
-       org                ; organize your plain life in plain text
+       (org +jupyter)                ; organize your plain life in plain text
        ;; php                ; perl's insecure younger brother
        ;; plantuml           ; diagrams for confusing people more
        ;; purescript         ; javascript, but functional
@@ -161,7 +161,7 @@
        ;; rest               ; Emacs as a REST client
        ;; rst                ; ReST in peace
        ;; (ruby +rails)      ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)        ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp +tree-sitter)        ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;; scala              ; java, but good
        ;; scheme             ; a fully conniving family of lisps
        (sh +tree-sitter)                ; she sells {ba,z,fi}sh shells on the C xor
@@ -170,7 +170,7 @@
        ;; swift              ; who asked for emoji variables?
        ;; terra              ; Earth and Moon in alignment for performance.
        ;; web                ; the tubes
-       yaml               ; JSON, but readable
+       (yaml +tree-sitter)               ; JSON, but readable
        ;; zig                ; C, but simpler
 
        :email
