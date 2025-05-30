c53f243e259d1c5bf8452d87e8516c390018ecd7
Typas Liao <typascake@gmail.com>
2025-05-30 19:11:52 +0800

    migrate from company to corfu

--- init.el
+++ init.el
@@ -24,7 +24,8 @@
        ;; tty                ; improve the terminal Emacs experience
 
        :completion
-       company            ; the ultimate code completion backend
+       ;; company            ; the ultimate code completion backend
+       (corfu +orderless)
        ;; helm               ; the *other* search engine for love and life
        ;; ido                ; the other *other* search engine...
        ivy                ; a search engine for love and life
@@ -66,7 +67,7 @@
        ;; objed              ; text object editing for the innocent
        ;; parinfer           ; turn lisp into python, sort of
        ;; rotate-text        ; cycle region at point between text candidates
-       snippets           ; my elves. They type so I don't have to
+       ;; snippets           ; my elves. They type so I don't have to
        ;; word-wrap          ; soft wrapping with language-aware indent
 
        :emacs
